483128efab0d1856020cf5e92bf00915376254b2
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-07 20:40:12 +0530

    appwrite auth

---
+++ blop-app/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../conf/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another function
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (err) {
+      console.log("Appwrite service :: getCurrentUser :: error", err);
+    }
+    return null;
+  }
+
+  async logOut(){
+    try{
+await this.account.deleteSession('current')
+    }catch(err){
+
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
